92c42a3d06c342f47a83a7e1fa35b7d558c63bf5
Vitalii Iakushev <vsiakushev@gmail.com>
2022-10-17 09:44:17 +0200

    Creating Only Portfolio App

---
+++ ViFinance Only Portfolio/PortfoliosViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PortfoliosViewController.swift
+//  ViFinance Only Portfolio
+//
+//  Created by Vitaliy Sergeevich on 16.10.2022.
+//  Copyright © 2022 Vitaliy Iakushev. All rights reserved.
+//
+
+import UIKit
+
+class PortfoliosViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
