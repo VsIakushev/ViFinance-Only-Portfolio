0de28802bd751ccf12376c49137df32c51ae8e4b
Vitalii Iakushev <vsiakushev@gmail.com>
2022-10-18 18:50:20 +0200

    Added ability to add "Done" button on NumPad-Keyboard (extension)

--- ViFinance Only Portfolio/PortfoliosViewController.swift
+++ ViFinance Only Portfolio/PortfoliosViewController.swift
@@ -62,16 +62,24 @@ class PortfoliosViewController: UIViewController {
     @IBOutlet weak var calculateButton: UIButton!
     @IBOutlet weak var tableView: UITableView!
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    } // скрытие клавиатуры после ввода
+    
     var stocksInPortfolio = PortfolioModel.getPortfolio()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         amountLabel.text = String(amountOfPortfolio)
-       
+        amountTextField.keyboardType = .numberPad
+        
         
         tableView.tableFooterView = UIView() //скрыл разлиновку таблицы ниже, последнего элемента портфеля.
     }
+    
+    
+    
     
 
     /*
@@ -85,13 +93,26 @@ class PortfoliosViewController: UIViewController {
     */
     
     @IBAction func calculatePressed(_ sender: UIButton) {
-        amountLabel.text = amountTextField.text ?? String(0)
-        amountOfPortfolio = Int(amountLabel.text!)!
+       
+        if let _ = Int(amountTextField.text!) {
+            amountLabel.text = amountTextField.text ?? String(0)
+            amountOfPortfolio = Int(amountLabel.text!)!
+            
+            stocksInPortfolio = PortfolioModel.getPortfolio()
+            tableView.reloadData()
+            // обновление таблицы после нажатия кнопки
+        } else {
+            let alert = UIAlertController(title: "Wrong format!", message: "Enter your portfolio amount", preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alert.addAction(okAction)
+            present(alert, animated: true, completion: nil)
+            print("Wrong amount format!")
+        }
         
-        stocksInPortfolio = PortfolioModel.getPortfolio()
-        tableView.reloadData()
+        
         
     }
+    
     
 }
 
@@ -108,10 +129,49 @@ extension PortfoliosViewController: UITableViewDelegate, UITableViewDataSource {
         cell.priceLabel.text = String(stocksInPortfolio[indexPath.row].price)
         cell.QuantityLabel.text = String(stocksInPortfolio[indexPath.row].quantity)
         
-        // вставить параметры, после создания модели
-        
         return cell
     }
     
     
 }
+
+
+// This extension turns on ability to add "Done" Button to NUMPad-keyboard.
+// Have to find out how to make connection between Done-button and calculations in class.
+
+extension UITextField{
+    @IBInspectable var doneAccessory: Bool{
+        get{
+            return self.doneAccessory
+        }
+        set (hasDone) {
+            if hasDone{
+                addDoneButtonOnKeyboard()
+            }
+        }
+    }
+
+    func addDoneButtonOnKeyboard()
+    {
+        let doneToolbar: UIToolbar = UIToolbar(frame: CGRect.init(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 50))
+        doneToolbar.barStyle = .default
+
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let done: UIBarButtonItem = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(self.doneButtonAction))
+
+        let items = [flexSpace, done]
+        doneToolbar.items = items
+        doneToolbar.sizeToFit()
+
+        self.inputAccessoryView = doneToolbar
+    }
+
+    @objc func doneButtonAction() {
+        
+        print("Done button action works")
+        
+        self.resignFirstResponder()
+        
+    }
+}
+
