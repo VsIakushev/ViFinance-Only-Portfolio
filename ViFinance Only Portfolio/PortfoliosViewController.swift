8a012b0dee5a178b93203ce21597f5bb83274e17
Vitalii Iakushev <vsiakushev@gmail.com>
2022-10-18 10:27:30 +0200

    Merged to one file. TableView update after new amount - done.

--- ViFinance Only Portfolio/PortfoliosViewController.swift
+++ ViFinance Only Portfolio/PortfoliosViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 
+var amountOfPortfolio = 15000
+
 class CustomCell: UITableViewCell {
     
     @IBOutlet weak var tickerLabel: UILabel!
@@ -17,9 +19,41 @@ class CustomCell: UITableViewCell {
     @IBOutlet weak var QuantityLabel: UILabel!
     
 }
+///////
 
+struct PortfolioModel {
+    var ticker : String
+    var share : Double
+    var amount : Double
+    var price : Double
+    var quantity : Int
 
+    
+    static  let arrayOfTickers = ["AAPL", "MSFT", "GOOG", "BRK.B", "META", "NVDA"]
+    static let arrayOfShares = [21.15, 17.22, 11.30, 6.55, 4.35, 3.47 ]
+    static var arrayOfAmounts = [0.0]
+    static  let arrayOfPrices = [153.12, 247.42, 120.57, 330.14, 140.32, 160.58]
+    static  let arrayOfNubmerOfStocks = [45, 22, 16, 13, 7, 6]
+    
+    static func getPortfolio() -> [PortfolioModel] {
+        var Portfolio = [PortfolioModel]()
+        arrayOfAmounts.removeAll()
+        
+        
+        for i in 0..<arrayOfTickers.count {
+            arrayOfAmounts.append(round((Double(amountOfPortfolio) * arrayOfShares[i] / 100)*10)/10)
+//            arrayOfAmounts.append((10000 * arrayOfShares[i]) / 100)
+        }
+        
+        for i in 0..<arrayOfTickers.count {
+            Portfolio.append(PortfolioModel(ticker: arrayOfTickers[i], share: arrayOfShares[i], amount: arrayOfAmounts[i], price: arrayOfPrices[i], quantity: Int(arrayOfAmounts[i] / arrayOfPrices[i])))
+            
+        }
+        return Portfolio
+    }
+}
 
+///////
 class PortfoliosViewController: UIViewController {
 
     
@@ -29,11 +63,11 @@ class PortfoliosViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     
     var stocksInPortfolio = PortfolioModel.getPortfolio()
-    var amountOfPortfolio = 0
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        amountLabel.text = String(amountOfPortfolio)
        
         
         tableView.tableFooterView = UIView() //скрыл разлиновку таблицы ниже, последнего элемента портфеля.
@@ -53,6 +87,10 @@ class PortfoliosViewController: UIViewController {
     @IBAction func calculatePressed(_ sender: UIButton) {
         amountLabel.text = amountTextField.text ?? String(0)
         amountOfPortfolio = Int(amountLabel.text!)!
+        
+        stocksInPortfolio = PortfolioModel.getPortfolio()
+        tableView.reloadData()
+        
     }
     
 }
